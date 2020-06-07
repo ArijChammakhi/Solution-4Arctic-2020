b0f7a60ddbb9b9da9314c01edf000e337e0f17a8
Amine BEN OMRANE <amine.benomrane@esprit.tn>
2020-06-07 11:42:48 +0100

    v2

---
+++ Solution.Data/Migrations/202006021932336_arouja.cs
@@ -0,0 +1,18 @@
+﻿namespace Solution.Data.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class arouja : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Meubles", "DatePublication", c => c.DateTime(nullable: false, precision: 7, storeType: "datetime2"));
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.Meubles", "DatePublication");
+        }
+    }
+}
