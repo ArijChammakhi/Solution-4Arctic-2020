98f850672d9e5f6adea8bee7f947a3bfe489d9a0
medchaib <47165439+medchaib@users.noreply.github.com>
2020-06-09 15:32:28 +0200

    Add files via upload

---
+++ Solution.Domain/Entities/formule.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solution.Domain.Entities
+{
+   public class formule
+    {
+        [Key]
+        public int id { get; set; }
+        public double salaire { get; set; }
+        public double montant_a_emprunter { get; set; }
+        public int nbr_mois { get; set; }
+    }
+}
